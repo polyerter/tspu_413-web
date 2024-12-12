49eba6155827e1fede0545f3b2bd427038197a1f
tspu_01 <tspu_01@mail.ru>
2024-12-12 15:51:15 +0700

    add category and view

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,24 @@ class Article(models.Model):
     text = models.CharField(
         max_length=1000,
     )
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        'Category', 
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    
+    def __str__(self):
+        return f'Статья: \'{self.title}\''
 
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=255,
+    )
+    
+    def __str__(self):
+        return f'Категория: \'{self.name}\''
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from blog.models import Article
 
-# Create your views here.
+
+
+def index(request):
+    data = Article.objects.all()
+
+    context = {
+        "data": data,
+    }
+
+    return render(request, "list.html", context)
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
